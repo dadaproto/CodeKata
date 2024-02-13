927cf7495e7f9a96700ece924040189ea2e76b45
Dadaproto <58527913+dadaproto@users.noreply.github.com>
2024-02-13 21:00:26 +0900

    update another query using join

--- SQL/2024-02-13-get-purchased-user-ratio.sql
+++ SQL/2024-02-13-get-purchased-user-ratio.sql
@@ -13,3 +13,32 @@ join online_sale s using(user_id)
 where year(joined) = 2021 
 group by 1, 2
 order by 1, 2
+
+
+
+-- join 사용 예시
+
+SELECT 
+    YEAR(s.sales_date) AS year, 
+    MONTH(s.sales_date) AS month,
+    COUNT(DISTINCT i.user_id) AS purchased_user,
+    ROUND(COUNT(DISTINCT i.user_id) / total_users.total_count, 1) AS purchased_ratio
+FROM 
+    user_info i
+JOIN 
+    online_sale s USING(user_id)
+JOIN 
+    (
+        SELECT 
+            COUNT(DISTINCT user_id) AS total_count
+        FROM 
+            user_info
+        WHERE 
+            YEAR(joined) = 2021
+    ) AS total_users ON 1=1
+WHERE 
+    YEAR(i.joined) = 2021 
+GROUP BY 
+    YEAR(s.sales_date), MONTH(s.sales_date)
+ORDER BY 
+    YEAR(s.sales_date), MONTH(s.sales_date);
